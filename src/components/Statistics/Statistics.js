d068e5ab8cde5fe7ea4a60db5a2c199cb5c335ab
rayhan3030 <rayhanddd@gmail.com>
2022-10-12 18:48:18 +0600

    statistics component and 404 error page added

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>This is Statisticsss</h2>
+            <button type="submit">Helo</button>
+        </div>
+    );
+};
+
+export default Statistics;
